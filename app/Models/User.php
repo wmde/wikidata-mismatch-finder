e00b67af286d57106fd4d54a43ae63ecf93321b0
Itamar Givon <itamar.givon@wikimedia.de>
2021-06-25 09:58:30 +0200

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,27 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+        'username',
+        'wiki_id'
     ];
 }

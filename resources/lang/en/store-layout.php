7a39fecba66aeb62a2bc105c8d113bc965287856
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-07-27 18:13:47 +0200

    Updated header to 'Mismatch Store' instead of 'Mismatch finder'

--- resources/lang/en/store-layout.php
+++ resources/lang/en/store-layout.php
@@ -9,7 +9,7 @@ return [
     |
     |
     */
-    'mismatch-finder' => 'Mismatch Finder',
+    'mismatch-finder' => 'Mismatch Store',
     'aria-labels:tabs' => 'Store Page Sections',
     'tab:authentication-token' => 'Authentication Token',
     'tab:import-status' => 'Import Status',

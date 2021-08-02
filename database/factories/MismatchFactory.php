4eb0ddef6c9103ff4ae16ca9816db3b6832e2e0d
Itamar Givon <itamar.givon.dev@gmail.com>
2021-08-02 11:58:27 +0200

    [Imports 0/7] Update mismatch migration (#35)
    This PR corrects the mismatch migration to fit the most minimal product requirements, keeping in mind length limitations. As it turns out, since we do not intend to do anything with the wikidata value besides showing it, there is no need to save it as a BLOB. In addition, since we limit the length of external values to 1500 then we do not need to store it as TEXT. In case any of these have to be updated in the future, we can create an additional migration to handle that.
    
    * Update mismatch migration according to requirements
    * Update fake value generation limits
    
    Bug: T285299

--- database/factories/MismatchFactory.php
+++ database/factories/MismatchFactory.php
@@ -32,14 +32,20 @@ class MismatchFactory extends Factory
 
     private function getRandomValue()
     {
+        $randomWordAmount = $this->faker->numberBetween(1, 5);
+        $randomDecimalLength = $this->faker->numberBetween(1, 25);
+
+        // Return one random value of any of the random value types below,
+        // to mimic data that might be in wikidata or external databases
         return $this->faker->randomElement([
+            // A random date
             $this->faker->date(),
-            $this->faker->randomFloat(),
-            $this->faker->randomNumber(),
-            $this->faker->words(
-                $this->faker->numberBetween(1, 15),
-                true
-            )
+            // A random floating point number with up to 30 digits
+            $this->faker->randomFloat($randomDecimalLength, 0, 10000),
+            // A random integer with up to 30 digits
+            $this->faker->randomNumber(30),
+            // A random lorem text with up to 5 words
+            $this->faker->words($randomWordAmount, true)
         ]);
     }
 }

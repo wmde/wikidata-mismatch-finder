6f62157a62eb1b23090a67929835185cc27f66ff
Itamar Givon <itamar.givon@wikimedia.de>
2021-08-02 11:53:36 +0200

    Add comments to explain the mismatch factory random value generator

--- database/factories/MismatchFactory.php
+++ database/factories/MismatchFactory.php
@@ -35,10 +35,16 @@ class MismatchFactory extends Factory
         $randomWordAmount = $this->faker->numberBetween(1, 5);
         $randomDecimalLength = $this->faker->numberBetween(1, 25);
 
+        // Return one random value of any of the random value types below,
+        // to mimic data that might be in wikidata or external databases
         return $this->faker->randomElement([
+            // A random date
             $this->faker->date(),
+            // A random floating point number with up to 30 digits
             $this->faker->randomFloat($randomDecimalLength, 0, 10000),
+            // A random integer with up to 30 digits
             $this->faker->randomNumber(30),
+            // A random lorem text with up to 5 words
             $this->faker->words($randomWordAmount, true)
         ]);
     }

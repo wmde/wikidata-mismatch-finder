5f16203a86d870010b023389b8d53feec03d4f8a
Itamar Givon <itamar.givon.dev@gmail.com>
2021-08-02 16:38:31 +0200

    [Imports 3/7] Create Wikidata API Client (#38)
    This change introduces a Wikibase API Client Service, so that we can call Wikidata API endpoints and cache our responses. The service already includes a method to call `wbparsevalue` which throws a `WikibaseValueParserException` on errors. Additionally, we wire up this service to the service container in `WikidataAPIProvider`, as it has external dependencies, and thus cannot be injected with Laravel's Zero-configuration resolution mechanism.
    
    This change also installs some caching middleware, in order to enable us to cache responses for our API calls to try to keep our call rate to a minimum.
    
    Bug: T285299

---
+++ tests/Feature/WikibaseAPIClientTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Http;
+use App\Services\WikibaseAPIClient;
+use Illuminate\Http\Client\Request;
+use Illuminate\Support\Str;
+use App\Exceptions\WikibaseValueParserException;
+use Illuminate\Http\Client\Response;
+use Mockery;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Arr;
+use Kevinrob\GuzzleCache\CacheMiddleware;
+use Kevinrob\GuzzleCache\Strategy\NullCacheStrategy;
+
+class WikibaseAPIClientTest extends TestCase
+{
+    const FAKE_API_URL = "http://fake.wikibase.api";
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_parse_value_returns_api_response(): void
+    {
+        $fakePayload = [
+            'property' => 'P1234',
+            'values' => 'fake-value'
+        ];
+        $fakeResponseBody = ['test' => 'okay'];
+
+        Http::fake(function (Request $req) use ($fakeResponseBody) {
+            return Http::response($fakeResponseBody, 200);
+        });
+
+        $mockCache = Mockery::mock(CacheMiddleware::class)->shouldIgnoreMissing();
+
+        $client = new WikibaseAPIClient(self::FAKE_API_URL, $mockCache);
+        $response = $client->parseValue($fakePayload['property'], $fakePayload['values']);
+
+        $this->assertActionRequest(self::FAKE_API_URL, 'wbparsevalue', $fakePayload);
+        $this->assertSame($fakeResponseBody, $response->json());
+    }
+
+    public function test_parse_value_throws_on_error_response()
+    {
+        $fakePayload = [
+            'property' => 'P1234',
+            'values' => 'fake-value'
+        ];
+        $fakeErrorResponse = ['error' => [
+            'info' => 'not okay'
+        ]];
+
+        Http::fake(function (Request $req) use ($fakeErrorResponse) {
+            return Http::response($fakeErrorResponse, 200);
+        });
+        $mockCache = Mockery::mock(CacheMiddleware::class)->shouldIgnoreMissing();
+
+        $this->expectException(WikibaseValueParserException::class);
+        $this->expectExceptionMessage($fakeErrorResponse['error']['info']);
+
+        $client = new WikibaseAPIClient(self::FAKE_API_URL, $mockCache);
+        $client->parseValue($fakePayload['property'], $fakePayload['values']);
+
+        $this->assertActionRequest(self::FAKE_API_URL, 'wbparsevalue', $fakePayload);
+    }
+
+    public function test_parse_value_retrieves_cached_responses()
+    {
+        $fakeResponseBody = ['test' => 'okay'];
+
+        $mockCache = Mockery::mock(CacheMiddleware::class);
+        // The `__invoke()` magic method is utilized by guzzle middleware:
+        // https://www.phptutorial.net/php-oop/php-__invoke/
+        $mockCache->shouldReceive('__invoke')->andReturn(function () use ($fakeResponseBody) {
+            return Http::response($fakeResponseBody, 200);
+        });
+
+        $client = new WikibaseAPIClient(self::FAKE_API_URL, $mockCache);
+        $response = $client->parseValue('P1234', 'fake-value');
+
+        $this->assertSame($fakeResponseBody, $response->json());
+    }
+
+    private function assertActionRequest(string $url, string $action, array $payload)
+    {
+        Http::assertSent(function (Request $req) use ($url, $action, $payload) {
+            foreach ($payload as $key => $value) {
+                if ($req[$key] != $value) {
+                    return false;
+                }
+            }
+
+            return Str::startsWith($req->url(), $url)
+                && $req['action'] == $action;
+        });
+    }
+}

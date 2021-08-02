e6bf017ac1e525917863efd79153782f176c8377
Itamar Givon <itamar.givon.dev@gmail.com>
2021-08-02 18:43:29 +0200

    Create mismatch CSV validation job (#24)
    This PR introduces the first job class to our code: `ValidateCSV`. The job is aimed at validating mismatch data in a CSV file prior to opening up a DB transaction and committing any mismatches to storage. In case a validation (or parsing) error is encountered, the job will fail and stop immediately, by throwing an `ImportValidationException`.
    
    Bug: T285299

---
+++ app/Jobs/ValidateCSV.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\ImportMeta;
+use Illuminate\Support\LazyCollection;
+use App\Exceptions\ImportValidationException;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use App\Rules\WikidataValue;
+use App\Services\CSVImportReader;
+use App\Exceptions\ImportParserException;
+use Throwable;
+
+class ValidateCSV implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Information about the current import.
+     *
+     * @var ImportMeta
+     */
+    protected $meta;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(ImportMeta $meta)
+    {
+        $this->meta = $meta;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(WikidataValue $valueValidator, CSVImportReader $reader)
+    {
+        try {
+            $filepath = Storage::disk('local')
+            ->path('mismatch-files/' . $this->meta->filename);
+
+            $reader->lines($filepath)
+                ->each(function ($mismatch, $i) use ($valueValidator) {
+                    $error = $this->checkFieldErrors($mismatch)
+                        ?? $this->checkValueErrors($mismatch, $valueValidator);
+
+                    if ($error) {
+                        throw new ImportValidationException($this->meta, $i, $error);
+                    }
+                });
+        } catch (Throwable $error) {
+            $this->meta->status = 'failed';
+            $this->meta->save();
+
+            throw $error;
+        }
+    }
+
+    private function checkFieldErrors($mismatch): ?string
+    {
+        $rules = config('mismatches.validation');
+
+        $validator = Validator::make($mismatch, [
+            'statement_guid' => [
+                'required',
+                'max:' . $rules['guid']['max_length'],
+                'regex:' . $rules['guid']['format']
+            ],
+            'property_id' => [
+                'required',
+                'max:' . $rules['pid']['max_length'],
+                'regex:' . $rules['pid']['format']
+            ],
+            'wikidata_value' => [
+                'required',
+                'max:' . $rules['wikidata_value']['max_length']
+            ],
+            'external_value' => [
+                'required',
+                'max:' . $rules['external_value']['max_length']
+            ],
+            'external_url' => [
+                'url',
+                'max:' . $rules['external_url']['max_length']
+            ]
+        ]);
+
+        if ($validator->stopOnFirstFailure()->fails()) {
+            return $validator->errors()->first();
+        }
+
+        return null;
+    }
+
+    private function checkValueErrors($mismatch, WikidataValue $valueRule): ?string
+    {
+        // We require a separate validator for wikidata value formatting,
+        // as it must be validated alongside the wikidata property id.
+        $validator = Validator::make([
+            'wikidata_value' => [
+                'property' => $mismatch['property_id'],
+                'value' => $mismatch['wikidata_value']
+            ]
+        ], [
+            'wikidata_value' => [$valueRule]
+        ]);
+
+        if ($validator->fails()) {
+            return $validator->errors()->first();
+        }
+
+        return null;
+    }
+}

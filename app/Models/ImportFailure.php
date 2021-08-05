98d63b121633801ac9657897e08929f71f1e2e1f
Itamar Givon <itamar.givon@wikimedia.de>
2021-08-05 09:35:23 +0200

    Create ImportFailure model

---
+++ app/Models/ImportFailure.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ImportFailure extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'line',
+        'message'
+    ];
+
+    public function importMeta()
+    {
+        return $this->belongsTo(ImportMeta::class, 'import_id')->withDefault();
+    }
+}

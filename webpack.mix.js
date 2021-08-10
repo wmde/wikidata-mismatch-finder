7508781bef81e640a3681f9f3f27a626eb39e126
Itamar Givon <itamar.givon@wikimedia.de>
2021-08-10 15:13:16 +0200

    Install TypeScript

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.ts('resources/js/app.ts', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 

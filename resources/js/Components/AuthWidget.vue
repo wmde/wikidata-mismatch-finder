fe7c4cb285a0adce73500864a3739906c00929ed
Itamar Givon <itamar.givon@wikimedia.de>
2021-08-11 14:44:33 +0200

    Make example component more dynamic w. ts

---
+++ resources/js/Components/AuthWidget.vue
@@ -0,0 +1,29 @@
+<template>
+    <div v-if="user" class="auth">
+        <!-- Stuff to show when user is logged in -->
+        <p>Hello, {{user.name}}!</p>
+    </div>
+    <div v-else class="guest">
+        <!-- Stuff to Show otherwise -->
+        <p>Hello, Guest!</p>
+    </div>
+</template>
+
+<script lang="ts">
+interface User {
+    name: string,
+    id: string
+}
+
+import Vue, { PropType } from 'vue';
+
+export default Vue.extend({
+    props: {
+        user: Object as PropType<User>
+    }
+});
+</script>
+
+<style lang="scss">
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return redirect(route('api.settings'));
 });
 
-Route::inertia('/test-inertia', 'SamplePage', [ 'user' => [ 'name' => 'Potato']]);
+Route::inertia('/test-inertia', 'Home', [ 'user' => [ 'name' => 'Potato']]);
 
 Route::get('/imports', function () {
     return view('importStatus', [ 'imports' => ImportMeta::with('error')->orderByDesc('id')->take(10)->get() ]);

8a6592a63972f7410b60b26a2042223978e67a6f
Itamar Givon <itamar.givon@wikimedia.de>
2021-08-19 17:39:28 +0200

    Implement i18n in client side app

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,14 +1,33 @@
-// import './bootstrap';
+import './bootstrap';
 import Vue from 'vue';
+import i18n from 'vue-banana-i18n';
 import { createInertiaApp } from '@inertiajs/inertia-vue';
-
-const app = document.getElementById('app');
+import i18nMessages from './lib/i18n';
+import Error from './Pages/Error.vue';
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
   setup({ el, app, props }) {
-    new Vue({
-      render: h => h(app, props),
-    }).$mount(el)
+      (async () => {
+        try {
+            const locale = document.documentElement.lang;
+            const messages = await i18nMessages(locale);
+
+            Vue.use(i18n, { locale, messages });
+
+            new Vue({
+                render: h => h(app, props),
+            }).$mount(el)
+        } catch (e) {
+            new Vue({
+                render: h => h(Error, {
+                    props: {
+                        title: 'Oops!',
+                        description: 'Something unexpected happened, but we are working on it!'
+                    }
+                }),
+            }).$mount(el)
+        }
+      })()
   },
 })

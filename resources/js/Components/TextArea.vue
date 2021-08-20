89727ee07e68205c932666e2c0ac302f0a545eb0
Itamar Givon <itamar.givon@wikimedia.de>
2021-08-20 17:02:45 +0200

    Update home page content

--- resources/js/Components/TextArea.vue
+++ resources/js/Components/TextArea.vue
@@ -10,7 +10,7 @@
 				{{ label }}
 			</label>
 		</span>
-    <textarea :class="[
+        <textarea :class="[
             'wikit-TextArea__textarea',
             checkValidLimit() ? `wikit-TextArea__textarea--${resize}` : 'wikit-TextArea__textarea--vertical'
         ]" :rows="rows" placeholder="Potato!!" label=""></textarea>
@@ -65,14 +65,89 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
-    // TODO: Add scrollbars
-    // TODO: Make component full width
-    // TODO: Add styles from tokens
+    @import '~@wmde/wikit-tokens/dist/_variables.scss';
+
+    @mixin Label($displayType: "inline") {
+        color: $wikit-Label-font-color;
+        font-family: $wikit-Label-font-family;
+        font-size: $wikit-Label-font-size;
+        font-weight: $wikit-Label-font-weight;
+        line-height: $wikit-Label-line-height;
+        display: $displayType;
+        overflow-wrap: break-word;
+        hyphens: auto;
+
+        &--disabled {
+            color: $wikit-Label-font-color-disabled;
+        }
+
+        @if $displayType == block {
+            padding-block-end: $wikit-Label-padding-block-end;
+        }
+    }
+
     .wikit-TextArea {
+        &__label-wrapper {
+            display: flex;
+            align-items: center;
+            gap: $dimension-spacing-small;
+        }
+
+        &__label {
+            @include Label("block");
+        }
+    }
+
+    .wikit-TextArea__textarea {
         display: block;
         width: 100%;
         // The default resizing behaviour should be on the y axis only
         resize: vertical;
+        padding-inline: $wikit-Input-desktop-padding-inline;
+        padding-block: $wikit-Input-desktop-padding-block;
+        box-shadow: inset 0 0 0 1px transparent; //TODO: replace by token
+
+        @media (max-width: $width-breakpoint-mobile) {
+            padding-inline: $wikit-Input-mobile-padding-inline;
+            padding-block: $wikit-Input-mobile-padding-block;
+        }
+
+        /**
+         */
+        border-color: $wikit-Input-border-color;
+        border-style: $wikit-Input-border-style;
+        border-width: $wikit-Input-border-width;
+        border-radius: $wikit-Input-border-radius;
+
+        background-color:  $wikit-Input-background-color;
+        color: $wikit-Input-color;
+
+        font-family: $wikit-Input-font-family;
+        font-size: $wikit-Input-font-size;
+        font-weight: $wikit-Input-font-weight;
+        line-height: $wikit-Input-line-height;
+
+        &:hover {
+            border-color: $wikit-Input-hover-border-color;
+        }
+
+        &:focus,
+        &:active {
+            border-color: $wikit-Input-active-border-color;
+            box-shadow: $wikit-Input-active-box-shadow;
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        &::placeholder {
+            font-family: $wikit-Input-placeholder-font-family;
+            font-size: $wikit-Input-placeholder-font-size;
+            font-weight: $wikit-Input-placeholder-font-weight;
+            line-height: $wikit-Input-placeholder-line-height;
+            color: $wikit-Input-placeholder-color;
+        }
 
         &--horizontal {
             resize: horizontal;

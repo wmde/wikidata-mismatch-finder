2c8104808a4b53f4db0a98b644c2b3b5ad4d8c6c
Itamar Givon <itamar.givon@wikimedia.de>
2021-08-25 16:17:20 +0200

    Ensure https urls in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         JsonResource::withoutWrapping();
+
+        // Ensure urls are generated with the correct scheme in production.
+        if (App::environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }

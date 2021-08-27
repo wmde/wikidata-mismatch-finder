2a2b6c4c853a7a47921a1c01a697f0c8a540f909
Itamar Givon <itamar.givon@wikimedia.de>
2021-08-27 12:41:14 +0200

    Add initial page markup and state logic

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,21 +1,55 @@
 <template>
-  <div>
+  <div class="page-container">
     <Head title="Mismatch Finder" />
-    <h1 class="h4">{{ $i18n('about-mismatch-finder-title') }}</h1>
-    <p id="about-description" >{{ $i18n('about-mismatch-finder-description') }}</p>
+    <section id="intro-section">
+        <h2 class="h4">{{ $i18n('about-mismatch-finder-title') }}</h2>
+        <p id="about-description" >{{ $i18n('about-mismatch-finder-description') }}</p>
+    </section>
+    <section id="querying-section">
+        <h2 class="h5">{{ $i18n('item-form-title') }}</h2>
+        <form id="item-form" @submit.prevent="send">
+            <text-area
+                :label="$i18n('item-form-id-input-label')"
+                :placeholder="$i18n('item-form-id-input-placeholder')"
+                :rows="8"
+                v-model="form.itemsInput"
+            />
+            <wikit-button native-type="submit" disabled>
+                {{ $i18n('item-form-submit') }}
+            </wikit-button>
+        </form>
+    </section>
   </div>
 </template>
 
 <script>
     import Vue from 'vue';
     import { Head } from '@inertiajs/inertia-vue';
+    import {
+        Button as WikitButton,
+        TextArea
+    } from '@wmde/wikit-vue-components';
 
     export default Vue.extend({
         components: {
-            Head
+            Head,
+            TextArea,
+            WikitButton
         },
         props: {
             user: Object,
+        },
+        methods: {
+            send(){
+                // TODO: Implement validation and form sending logic (See: https://inertiajs.com/forms)
+            }
+        },
+        data(){
+            return {
+                form: {
+                    itemsInput: ''
+                }
+            }
         }
     });
 </script>

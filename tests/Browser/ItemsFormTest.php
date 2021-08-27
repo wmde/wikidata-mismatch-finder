db5f0f5922e95d820be6b89ce46a3bea129b7028
Itamar Givon <itamar.givon@wikimedia.de>
2021-08-27 18:16:56 +0200

    Add browser test to cover BDD specs

---
+++ tests/Browser/ItemsFormTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+use Tests\Browser\Pages\HomePage;
+
+class ItemsFormTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function test_can_enter_list_of_item_ids()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new HomePage)
+                    ->keys('@items-input', 'Q1', '{return_key}', 'Q2')
+                    ->assertInputValue('@items-input', "Q1\nQ2");
+        });
+    }
+}

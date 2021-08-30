aee8dc75cfe83416a3d1804869e9c322d65c0323
Itamar Givon <itamar.givon.dev@gmail.com>
2021-08-30 12:44:35 +0200

    Add querying section to home page (#82)
    This change adds the querying section to the home page of the Mismatch Finder application. This section contains a form to send item IDs to the server, which will be used to filter mismatches to retrieve. The submit button is currently disabled, as we cannot yet display the results of the form.
    
    * Add messages
    * Add typographic mixins
    * Add initial page markup and state logic
    * Apply typography and layout styles
    * Apply page specific styles
    * Add browser test to cover BDD specs
    
    Bug: T289057

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,27 +1,97 @@
 <template>
-  <div>
+  <div class="page-container">
     <Head title="Mismatch Finder" />
-    <h1 class="h4">{{ $i18n('about-mismatch-finder-title') }}</h1>
-    <p id="about-description" >{{ $i18n('about-mismatch-finder-description') }}</p>
+    <section id="intro-section">
+        <h2 class="h4">{{ $i18n('about-mismatch-finder-title') }}</h2>
+        <p id="about-description" >{{ $i18n('about-mismatch-finder-description') }}</p>
+    </section>
+    <section id="querying-section">
+        <h2 class="h5">{{ $i18n('item-form-title') }}</h2>
+        <form id="items-form" @submit.prevent="send">
+            <text-area
+                :label="$i18n('item-form-id-input-label')"
+                :placeholder="$i18n('item-form-id-input-placeholder')"
+                :rows="8"
+                v-model="form.itemsInput"
+            />
+            <div class="form-buttons">
+                <wikit-button native-type="submit" disabled>
+                    {{ $i18n('item-form-submit') }}
+                </wikit-button>
+            </div>
+        </form>
+    </section>
   </div>
 </template>
 
 <script>
     import Vue from 'vue';
     import { Head } from '@inertiajs/inertia-vue';
+    import {
+        Button as WikitButton,
+        TextArea
+    } from '@wmde/wikit-vue-components';
 
     export default Vue.extend({
         components: {
-            Head
+            Head,
+            TextArea,
+            WikitButton
         },
         props: {
             user: Object,
+        },
+        methods: {
+            send(){
+                // TODO: Implement validation and form sending logic (See: https://inertiajs.com/forms)
+            }
+        },
+        data(){
+            return {
+                form: {
+                    itemsInput: ''
+                }
+            }
         }
     });
 </script>
 
 <style lang="scss">
+@import '~@wmde/wikit-tokens/dist/_variables.scss';
+
 #about-description {
     max-width: 705px;
+}
+
+#items-form {
+    /**
+    * Colors
+    */
+    background-color: $background-color-neutral-default;
+
+    /**
+    * Border
+    */
+    border-style: $border-style-base;
+    border-width: $border-width-thin;
+    border-color: $border-color-base-subtle;
+    border-radius: $border-radius-base;
+
+    /**
+    * Layout
+    */
+    padding: $dimension-spacing-large;
+    margin: $dimension-layout-xsmall 0;
+
+    // Any direct decendent of this form that has a predecessor element will
+    // get a top margin, this creates the even gutter between elements or "stack"
+    // See https://every-layout.dev/layouts/stack/#the-solution
+    & > * + * {
+        margin-top: $dimension-spacing-large;
+    }
+
+    .form-buttons {
+        text-align: end;
+    }
 }
 </style>
---
+++ tests/Browser/Pages/HomePage.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Browser\Pages;
+
+use Laravel\Dusk\Browser;
+use Laravel\Dusk\Page;
+
+class HomePage extends Page
+{
+    /**
+     * Get the URL for the page.
+     *
+     * @return string
+     */
+    public function url()
+    {
+        return route('home');
+    }
+
+    /**
+     * Assert that the browser is on the page.
+     *
+     * @param  Browser  $browser
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser
+            ->assertPathIs('/')
+            ->assertPresent('@form');
+    }
+
+    /**
+     * Get the element shortcuts for the page.
+     *
+     * @return array
+     */
+    public function elements()
+    {
+        return [
+            '@form' => '#items-form',
+            '@items-input' => '@form textarea'
+        ];
+    }
+}

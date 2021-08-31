dd4909294f6715a6f8f01c1b095049019b67283a
sihe <silvan.heintze@wikimedia.de>
2021-08-31 14:08:20 +0200

    Add forgotten log channel for mismatch_updates

--- config/logging.php
+++ config/logging.php
@@ -47,6 +47,12 @@ return [
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
+        'mismatch_updates' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/mismatch_updates.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+        ],
+
         'daily' => [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),

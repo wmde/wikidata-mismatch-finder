834e56c40a0bee37a9d717387578a0eccdd454d3
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-09-07 15:28:57 +0200

    Use Link component instead of a html tag

--- resources/js/Components/AuthWidget.vue
+++ resources/js/Components/AuthWidget.vue
@@ -1,13 +1,12 @@
 <template>
     <div>
-        <div v-if="user">
-            <a :href="`https://www.wikidata.org/wiki/User:${user.name}`">
+        <div class="top-right-nav" v-if="user">
+            <Link :href="`https://www.wikidata.org/wiki/User:${user.name}`">
                 <img src="../../img/user.svg" class="icon-user" /><span class="username">{{ user.name }}</span>
-                </a>
-            <a href="/auth/logout">{{ $i18n('log-out') }}</a>
-            
+            </Link>
+            <Link href="/auth/logout">{{ $i18n('log-out') }}</Link>      
         </div>
-        <a v-else href="/auth/login">{{ $i18n('log-in') }}</a>
+        <Link v-else href="/auth/login">{{ $i18n('log-in') }}</Link>
     </div>
 </template>
 
@@ -15,10 +14,21 @@
 
 import Vue, { PropType } from 'vue';
 import User from '../types/User';
+import { Link } from '@wmde/wikit-vue-components';
 
 export default Vue.extend({
     props: {
         user: Object as PropType<User>
+    },
+    components: {
+        Link
     }
 });
 </script>
+
+<style lang="scss">
+
+.top-right-nav {
+    display: flex;
+}
+</style>

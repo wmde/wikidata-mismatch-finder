88c0cfad296195b66cd7fc5da6b6502004ffdfba
sihe <silvan.heintze@wikimedia.de>
2021-09-08 11:55:40 +0200

    Add website header to Results page
    Bug: T287571

--- resources/js/Pages/Results.vue
+++ resources/js/Pages/Results.vue
@@ -1,23 +1,28 @@
 <template>
-  <div>
-    <Head title="Mismatch Finder - Results" />
-    <h1>Mismatch Finder - Results!</h1>
-    <p class="results">
-        Thank you for sending IDs {{item_ids}}. Mismatch results will be displayed in this page soon.
-    </p>
-  </div>
+    <Layout :user="user">
+        <div>
+            <Head title="Mismatch Finder - Results" />
+            <h3>Results</h3>
+            <p class="results">
+                Thank you for sending IDs {{item_ids}}. Mismatch results will be displayed in this page soon.
+            </p>
+        </div>
+  </Layout>
 </template>
 
 <script>
     import Vue from 'vue';
     import { Head } from '@inertiajs/inertia-vue'
+    import Layout from './Layout';
 
     export default Vue.extend({
         components: {
-            Head
+            Head,
+            Layout
         },
         props: {
-            item_ids: Array
+            item_ids: Array,
+            user: Object
         }
     });
 </script>

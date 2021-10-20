95558e28af9851f8f02f78901ff61c56f0811205
Itamar Givon <itamar.givon@wikimedia.de>
2021-10-20 17:06:15 +0200

    Add initial styles

--- resources/js/Components/Dialog.vue
+++ resources/js/Components/Dialog.vue
@@ -1,20 +1,26 @@
 <template>
-    <section :class="[
+    <div :class="[
         'wikit',
         'wikit-Dialog'
     ]">
-        <header>{{title}}</header>
-        <footer>
-            <wikit-button v-for="(action, i) in actions"
-                :key="i"
-                :class="[action.namespace]"
-                :variant="i === 0 ? 'primary' : 'normal'"
-                :type="i === 0 ? 'progressive' : 'neutral'"
-            >
-                {{action.label}}
-            </wikit-button>
-        </footer>
-    </section>
+        <div class="wikit-Dialog__overlay"></div>
+        <div class="wikit-Dialog__modal">
+            <header class="wikit-Dialog__header">{{title}}</header>
+            <footer class="wikit-Dialog__footer">
+                <wikit-button v-for="(action, i) in actions"
+                    :key="i"
+                    :class="[
+                        'wikit-Dialog__action',
+                        action.namespace
+                    ]"
+                    :variant="i === 0 ? 'primary' : 'normal'"
+                    :type="i === 0 ? 'progressive' : 'neutral'"
+                >
+                    {{action.label}}
+                </wikit-button>
+            </footer>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -38,7 +44,7 @@ export default defineComponent({
             required: true
         },
         actions: {
-            type: Object as PropType<DialogAction[]>,
+            type: Array as PropType<DialogAction[]>,
             required: true
         }
     }
@@ -46,5 +52,113 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+    @import '~@wmde/wikit-tokens/dist/_variables.scss';
+    $base: '.wikit-Dialog';
 
+    #{$base} {
+        /**
+        * Layout
+        */
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+
+    #{$base}__overlay {
+        /**
+        * Layout
+        */
+        width: $wikit-Dialog-overlay-width;
+        height: $wikit-Dialog-overlay-height;
+        position: absolute;
+        top: 0;
+        left: 0;
+
+        /**
+        * Colors
+        */
+        background-color: $wikit-Dialog-overlay-background-color;
+        opacity: $wikit-Dialog-overlay-opacity;
+
+        // $wikit-Dialog-overlay-transition-duration: 250ms;
+    }
+
+    #{$base}__modal {
+        /**
+        * Layout
+        */
+        width: $wikit-Dialog-width-complex;
+        max-width: 75%;
+        max-height: 90%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        /**
+        * Colors
+        */
+        color: $wikit-Dialog-body-color;
+        background-color: $wikit-Dialog-background-color;
+
+        /**
+        * Typography
+        */
+        font-family: $wikit-Dialog-body-font-family;
+        font-size: $wikit-Dialog-body-font-size;
+        font-weight: $wikit-Dialog-body-font-weight;
+        line-height: $wikit-Dialog-body-line-height;
+
+        /**
+        * Borders
+        */
+        border-style: $wikit-Dialog-border-style;
+        border-width: $wikit-Dialog-border-width;
+        border-radius: $wikit-Dialog-border-radius;
+        box-shadow: $wikit-Dialog-elevation;
+
+        // $wikit-Dialog-transition-duration: 250ms;
+    }
+
+    #{$base}__header {
+        color: $wikit-Dialog-header-color;
+
+        font-family: $wikit-Dialog-header-font-family;
+        font-size: $wikit-Dialog-header-font-size;
+        font-weight: $wikit-Dialog-header-font-weight;
+        line-height: $wikit-Dialog-header-line-height;
+
+        padding-block-start: $wikit-Dialog-header-spacing-top;
+        padding-block-end: $wikit-Dialog-header-spacing-bottom-complex;
+        padding-inline-start: $wikit-Dialog-header-spacing-left;
+        padding-inline-end: $wikit-Dialog-header-spacing-left;
+
+        // $wikit-Dialog-header-box-shadow: inset 0 1px 0 0 #c8ccd1; // only for complex dialogs: divider to be displayed when scroll is activated
+    }
+
+    #{$base}__content {
+        padding-block-start: $wikit-Dialog-body-spacing-top-complex;
+        padding-block-end: $wikit-Dialog-body-spacing-bottom-complex;
+        padding-inline-start: $wikit-Dialog-body-spacing-left;
+        padding-inline-end: $wikit-Dialog-body-spacing-left;
+    }
+
+    #{$base}__footer {
+        box-shadow: $wikit-Dialog-footer-box-shadow;
+
+        padding-block-start: $wikit-Dialog-footer-spacing-top-complex;
+        padding-inline-start: $wikit-Dialog-footer-spacing-left;
+        padding-inline-end: $wikit-Dialog-footer-spacing-left;
+
+        display: flex;
+        flex-direction: row-reverse;
+        flex-wrap: wrap;
+
+        #{$base}__action {
+            margin-block-end: $wikit-Dialog-footer-spacing-bottom-complex;
+            margin-inline-end: $wikit-Dialog-footer-spacing;
+        }
+    }
 </style>

32db2db6f2c84fc17a1b2006a7e4fc1572acd24c
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-11-08 14:49:03 +0100

    Fix target _blank

--- resources/js/Components/MismatchRow.vue
+++ resources/js/Components/MismatchRow.vue
@@ -3,7 +3,7 @@
         <td :data-header="$i18n('column-property')">
             <wikit-link
               class="break-line-link"
-              target="<span class="x x-first x-last">_blank</span>"
+              target="_blank"
               :href="`https://www.wikidata.org/wiki/Property:${mismatch.property_id}`">
                 {{mismatch.property_label}}
             </wikit-link>

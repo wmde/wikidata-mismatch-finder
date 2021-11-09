deba7275131db0d2f1528827d18fc7a81a4db144
Silvan Heintze <59574251+Silvan-WMDE@users.noreply.github.com>
2021-11-09 14:33:57 +0100

    suggested test (#197)
    Thanks!

--- tests/Vue/Pages/Home.spec.js
+++ tests/Vue/Pages/Home.spec.js
@@ -1,11 +1,10 @@
-import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import Vuex from 'vuex'
 import Home from '@/Pages/Home.vue';
 
 // Stub the inertia vue components module entirely so that we don't run into
 // issues with the Head component.
 jest.mock('@inertiajs/inertia-vue', () => ({}));
-
 
 describe('Home.vue', () => {
 
@@ -17,58 +16,23 @@ describe('Home.vue', () => {
     }
 
     const localVue = createLocalVue();
-
     localVue.use(Vuex);
 
-    // let windowSpy;
+    const store = new Vuex.Store();
 
-    // beforeEach(() => {
-    //   windowSpy = jest.spyOn(window, "window", "get");
-    // });
-    
-    // afterEach(() => {
-    //   windowSpy.mockRestore();
-    // });
+    it('sanitises input with empty lines', async () => {
+        const formData = { itemsInput: 'Q1\n\nQ2\n' };
 
-    it('goes to Results page when Check items button is clicked', async () => {
-
-        const inertiaGet = jest.fn();
-
-        const store = new Vuex.Store();
         const wrapper = mount(Home, { 
             mocks,
             localVue,
             store,
-            $inertia: { get: inertiaGet },
             data() {
-                return {
-                    form: {
-                        itemsInput: 'Q1\nQ2'
-                    }
-                }
+                return { form: formData };
             },
         });
 
-        // windowSpy.mockImplementation(() => ({
-        //     location: {
-        //       origin: "/results?ids=Q1|Q2"
-        //     }
-        //   }));
-
-        // const itemsInput = wrapper.find('textarea');
-        // await itemsInput.setValue('Q5\nQ6');
-
-        // window.location = {
-        //     ...window.location,
-        //     reload: jest.fn()
-        // }
-
-        //window.location.assign = jest.fn();
-        await wrapper.find('.wikit-Button[type="submit"]').trigger('click');
-        //expect(window.location.origin).toHaveBeenCalledWith('/results?ids=Q1');
-        //expect(wrapper.vm.serializeInput)
-        expect( inertiaGet ).toHaveBeenCalledWith('/results?ids=Q1|Q2');
-
+        expect( wrapper.vm.serializeInput() ).toEqual('Q1|Q2');
     });
 
 })

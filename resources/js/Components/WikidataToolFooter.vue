3326e53b752a7cf392505585c7d80041ceda7534
Itamar Givon <itamar.givon@wikimedia.de>
2021-11-18 16:03:24 +0100

    Componentize footer for abstraction
    This abstraction is created in order to be able to reuse this
    footer in future potential tools, and mybe in a common
    wikidata tool layout in the far future

---
+++ resources/js/Components/WikidataToolFooter.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="footer-container">
+        <footer class="content-wrap">
+            <section>
+                <h2 class="h5">About Mismatch Finder</h2>
+                <p>Licensed under <a href="#">BSD 3-Clause License</a></p>
+                <p><a href="#">View Source</a></p>
+                <p><a href="#">Report an issue</a></p>
+            </section>
+            <section>
+                <h2 class="h5">About us</h2>
+                <p><a href="#">Privacy Policy</a></p>
+                <p><a href="#">Wikimedia Deutschland</a></p>
+                <p><a href="#">Made by the Wikidata team</a></p>
+            </section>
+            <slot />
+        </footer>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { Link as WikitLink } from '@wmde/wikit-vue-components';
+
+export default Vue.extend({
+});
+</script>
+
+<style lang="scss">
+@import '~@wmde/wikit-tokens/dist/_variables.scss';
+
+.footer-container {
+    background-color: $color-base-90;
+
+    & > footer {
+        margin: auto;
+        padding: $dimension-layout-xsmall;
+        display: flex;
+        flex-direction: column;
+
+        .h5 {
+            margin: 0 0 $dimension-layout-xsmall 0;
+        }
+
+        section {
+            margin-block-end: $dimension-layout-small;
+        }
+
+        p {
+            margin-block-end: 0;
+        }
+
+        p + p {
+            margin-block-start: $dimension-layout-xxsmall;
+        }
+
+        @media (min-width: $width-breakpoint-tablet) {
+            flex-direction: row;
+            padding: $dimension-layout-small;
+
+            & section {
+                margin-inline-end: $dimension-layout-large;
+            }
+        }
+    }
+}
+</style>
--- resources/js/Pages/Layout.vue
+++ resources/js/Pages/Layout.vue
@@ -10,41 +10,31 @@
             <h1>{{ $i18n('mismatch-finder-title') }}</h1>
             <slot />
         </main>
-        <div class="footer-container">
-            <footer class="content-wrap">
-                <section>
-                    <h2 class="h5">About Mismatch Finder</h2>
-                    <p>Licensed under <a href="#">BSD 3-Clause License</a></p>
-                    <p><a href="#">View Source</a></p>
-                    <p><a href="#">Report an issue</a></p>
-                </section>
-                <section>
-                    <h2 class="h5">About us</h2>
-                    <p><a href="#">Privacy Policy</a></p>
-                    <p><a href="#">Wikimedia Deutschland</a></p>
-                    <p><a href="#">Made by the Wikidata team</a></p>
-                </section>
-                <section>
-                    <h2 class="h5">More Data Quality Tools</h2>
-                    <p><a href="#">Query Builder</a></p>
-                    <p><a href="#">Item Quality Evaluator</a></p>
-                    <p><a href="#">Curious Facts</a></p>
-                    <p><a href="#">Constraint Violation Checker</a></p>
-                </section>
-            </footer>
-        </div>
+        <wikidata-tool-footer>
+            <section>
+                <h2 class="h5">More Data Quality Tools</h2>
+                <p><a href="#">Query Builder</a></p>
+                <p><a href="#">Item Quality Evaluator</a></p>
+                <p><a href="#">Curious Facts</a></p>
+                <p><a href="#">Constraint Violation Checker</a></p>
+            </section>
+        </wikidata-tool-footer>
     </div>
 </template>
 
 <script lang="ts">
     import { PropType } from 'vue';
     import AuthWidget from '../Components/AuthWidget.vue';
+    import WikidataToolFooter from '../Components/WikidataToolFooter.vue';
 
     import defineComponent from '../types/defineComponent';
     import User from '../types/User';
 
     export default defineComponent({
-        components: { AuthWidget },
+        components: {
+            AuthWidget,
+            WikidataToolFooter
+        },
         props: {
             user: Object as PropType<User>
         }
@@ -63,53 +53,17 @@
         margin-block-end: $dimension-layout-small;
     }
 
-    .footer-container {
-        background-color: $color-base-90;
-
-        & > footer {
-            margin: auto;
-            padding: $dimension-layout-xsmall;
-            display: flex;
-
-            .h5 {
-                margin: 0 0 $dimension-layout-xsmall 0;
-            }
-
-            section {
-                margin-block-end: $dimension-layout-small;
-            }
-
-            p {
-                margin-block-end: 0;
-            }
-
-            p + p {
-                margin-block-start: $dimension-layout-xxsmall;
-            }
-        }
-    }
-
-    main > header,
-    .footer-container > footer {
+    main > header {
         flex-direction: column;
     }
 
     @media (min-width: $width-breakpoint-tablet) {
-        main > header,
-        .footer-container > footer {
+        main > header {
             flex-direction: row;
         }
 
         .wikidata-logo {
             margin-block-end: 0;
-        }
-
-        .footer-container > footer {
-            padding: $dimension-layout-small;
-
-            section {
-                margin-inline-end: $dimension-layout-large;
-            }
         }
     }
 }

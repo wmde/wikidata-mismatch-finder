2adbc77840c80e0191c65db4f607973630d815d4
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-11-23 15:12:24 +0100

    Add Schema:FillableMismatch

---
+++ app/Virtual/Models/FillableMismatch.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @OA\Schema(
+ *      schema="FillableMismatch",
+ *      @OA\Property(
+ *          property="review_status",
+ *          type="string",
+ *          enum={"pending", "wikidata", "external", "both", "none"}
+ *      )
+ * )
+ */

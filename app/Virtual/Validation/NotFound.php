00e59e39d0b1b55ef9d917be9e72ffab4cdcf8f3
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-11-23 17:54:33 +0100

    Add Schema:NotFound

---
+++ app/Virtual/Validation/NotFound.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @OA\Schema(
+ *      schema="NotFound",
+ *      title="NotFound",
+ *      description="The specified resource was not found",
+ *      required={"message"},
+ *      @OA\MediaType(
+ *          mediaType="application/json"
+ *      ),
+ *      @OA\Property(
+ *          property="message",
+ *          type="string"
+ *      )
+ * )
+ */

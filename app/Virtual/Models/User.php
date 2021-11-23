1c7aa0bd79afb1f37384d0d4a1f4afc55fe7ac8b
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2021-11-23 15:08:35 +0100

    Add Schema:User

---
+++ app/Virtual/Models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @OA\Schema(
+ *      schema="User",
+ *      @OA\Property(
+ *          property="id",
+ *          type="number"
+ *      ),
+ *      @OA\Property(
+ *          property="username",
+ *          type="string"
+ *      ),
+ *      @OA\Property(
+ *          property="mw_userid",
+ *          type="number"
+ *      )
+ * )
+ */

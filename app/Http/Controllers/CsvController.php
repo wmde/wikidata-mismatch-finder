58cfe715b7630911dd6a626f59664f79739d3725
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2022-01-10 18:26:47 +0100

    Add data for all columns

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -56,17 +56,27 @@ class CsvController extends Controller
         foreach ($imports as $each_import) {
             $mismatches = Mismatch::where('import_id', $each_import->id)->get();
             $mismatches_count = $mismatches->count();
+            $review_statuses = array( 'wikidata', 'external', 'both','none', 'pending' );
+            $percent_completed = 0;
 
+            foreach ($review_statuses as $review_status) {
+                ${"mismatches_" . $review_status} = $mismatches->where('review_status', $review_status)->count();
+            }
+
+            if ($mismatches_count > 0) {
+                $percent_completed = (($mismatches_count - $mismatches_pending) / $mismatches_count) * 100;
+            }
+    
             fputcsv($handle, [
                 $each_import->id,
                 $each_import->status,
                 $mismatches_count,
-                100,
-                100,
-                100,
-                100,
-                100,
-                100
+                $mismatches_wikidata,
+                $mismatches_external,
+                $mismatches_both,
+                $mismatches_none,
+                $mismatches_pending,
+                $percent_completed
             ]);
         }
         fclose($handle);

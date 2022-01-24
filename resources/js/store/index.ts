719130e68096c06f590ef78f89bb51835bd4e3b8
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2022-01-24 11:35:34 +0100

    Extract state to getInitialState function

--- resources/js/store/index.ts
+++ resources/js/store/index.ts
@@ -2,14 +2,18 @@ import Vue from 'vue';
 import Vuex, {Store} from 'vuex';
 import { Inertia } from '@inertiajs/inertia';
 
+export function getInitialState() {
+    return {
+        loading: false,
+        lastSearchedIds: ''
+    }
+}
+
 export function createStore(): Store<{loading: boolean}>{
     Vue.use(Vuex);
 
     const store = new Store({
-        state: {
-            loading: false,
-            lastSearchedIds: ''
-        },
+        state: getInitialState(),
         mutations: {
             startLoader (state) {
                 state.loading = true;

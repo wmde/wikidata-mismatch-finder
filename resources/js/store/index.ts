6a8d31cdfbd5ef2d9bb16e16b8d63666a0f31ff1
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2022-01-24 11:22:08 +0100

    Extract createStore to store/index.ts

---
+++ resources/js/store/index.ts
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+import Vuex, {Store} from 'vuex';
+import { Inertia } from '@inertiajs/inertia';
+
+export function createStore(): Store<{loading: boolean}>{
+    Vue.use(Vuex);
+
+    const store = new Store({
+        state: {
+            loading: false,
+            lastSearchedIds: ''
+        },
+        mutations: {
+            startLoader (state) {
+                state.loading = true;
+            },
+            stopLoader (state) {
+                state.loading = false;
+            },
+            saveSearchedIds (state, searchedIds) {
+                state.lastSearchedIds = searchedIds;
+            }
+        }
+    });
+
+    let timer: ReturnType<typeof setTimeout>;
+
+    Inertia.on('start', () => {
+        // Only instantiate loading state after 250ms. This is done to
+        // prevent a flash of the loader in case loading is nearly
+        // immediate, which can be visually distracting.
+        timer = setTimeout(() => store.commit('startLoader'), 250);
+    });
+
+    Inertia.on('finish', (event) => {
+        clearTimeout(timer);
+        const status = event.detail.visit;
+
+        if (status.completed || status.cancelled) {
+            store.commit('stopLoader');
+        }
+    });
+
+    return store;
+}

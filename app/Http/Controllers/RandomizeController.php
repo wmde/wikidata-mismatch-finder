c058ed352e82dce80f0126f87de9e4f00625dbfd
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2022-10-12 10:40:25 +0200

    Add controller and endpoint for retrieving 15 random mismatches
    Bug: T302289

---
+++ app/Http/Controllers/RandomizeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mismatch;
+use App\Services\StatsdAPIClient;
+
+class RandomizeController extends Controller
+{
+    use Traits\ReviewMismatch, Traits\StatsTracker;
+
+    /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(StatsdAPIClient $statsd)
+    {
+        $this->middleware('simulateError');
+        $this->statsd = $statsd;
+    }
+
+    public function index()
+    {
+        $itemIds = Mismatch::with('importMeta.user')
+            ->distinct()
+            ->where('review_status', 'pending')
+            ->whereHas('importMeta', function ($import) {
+                $import->where('expires', '>=', now());
+            })
+            ->inRandomOrder()
+            ->limit(15)
+            ->pluck('item_id')->toArray();
+        
+        if (count($itemIds) > 0) {
+            $itemIdsToString = implode('|', $itemIds);
+
+            return redirect()->action(
+                [ResultsController::class, 'index'],
+                ['ids' => $itemIdsToString]
+            );
+        }
+    }
+}

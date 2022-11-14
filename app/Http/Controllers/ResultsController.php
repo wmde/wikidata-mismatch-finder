wmde/wikidata-mismatch-finder

commit e7fd105ed49f642efffd9493af1b290165fb7738

Get property datatypes in ResultsController (#444)
This is mainly in preparation for parsing and formatting time values,
but we can also use it to partially solve T321214, since we now know
which properties contain item IDs instead of inferring it from the
value. (But note that it doesn’t fully fix T321214, since addLabels() in
Results.vue still adds labels to all values regardless of property data
type.)

In the tests, we need to update the factory so that it generates random
Wikidata values matching the randomly selected property ID (previously,
tests still worked if the two didn’t match). In ResultsController /
WebResultsRouteTest, we go one step further and test three different
mismatches with specific data types: wikibase-item (should fetch the
label), time (no special handling yet, but should later be formatted),
and string (no special handling).

app/Http/Controllers/ResultsController.php
@@ -44,7 +44,13 @@ class ResultsController extends Controller
             })
             ->lazy();
 
-        $entityIds = $this->extractEntityIds($mismatches, $itemIds);
+        $propertyIds = $this->extractPropertyIds($mismatches);
+        $datatypes = $wikidata->getPropertyDatatypes($propertyIds);
+
+        $entityIds = array_merge(
+            $propertyIds,
+            $this->extractItemIds($mismatches, $datatypes)
+        );
 
         $props = array_merge(
             [
@@ -62,29 +68,29 @@ class ResultsController extends Controller
         return inertia('Results', $props);
     }
 
-    private function extractEntityIds(LazyCollection $mismatches, array $initialIds): array
+    private function extractPropertyIds(LazyCollection $mismatches): array
     {
-        $entityIdExtractor = function (array $ids, Mismatch $mismatch): array {
-            $wikidataValue = $mismatch->wikidata_value;
-            $entityValue = preg_match(config('mismatches.validation.item_id.format'), $wikidataValue);
+        return array_keys($mismatches->reduce(
+            function (array $ids, Mismatch $mismatch): array {
+                $ids[$mismatch->property_id] = null;
+                return $ids;
+            },
+            []
+        ));
+    }
 
-            // Add any new property id to the array of ids.
-            if (!in_array($mismatch->property_id, $ids)) {
-                $ids[] = $mismatch->property_id;
-            }
-
-            // If the wikidata value is an item id, add it to the array of ids if
-            // it is not there yet.
-            if ($entityValue && !in_array($wikidataValue, $ids)) {
-                $ids[] = $wikidataValue;
-            }
-
-            return $ids;
-        };
-
-        // Extract all entity ids encountered in mismatch data, and add them
-        // into an array of initial entity ids.
-        return $mismatches->reduce($entityIdExtractor, $initialIds);
+    private function extractItemIds(LazyCollection $mismatches, array $datatypes): array
+    {
+        return array_keys($mismatches->reduce(
+            function (array $ids, Mismatch $mismatch) use ($datatypes) {
+                $ids[$mismatch->item_id] = null;
+                if ($datatypes[$mismatch->property_id] === 'wikibase-item') {
+                    $ids[$mismatch->wikidata_value] = null;
+                }
+                return $ids;
+            },
+            []
+        ));
     }
 
     /**

tests/Feature/WebResultsRouteTest.php
@@ -59,44 +59,86 @@ class WebResultsRouteTest extends TestCase
         ->for(User::factory()->uploader())
         ->create();
 
-        $mismatch = Mismatch::factory()
+        $dateMismatch = Mismatch::factory()
             ->for($import)
+            ->datatypeTime()
             ->create();
+        $dateMismatchQid = $dateMismatch->item_id;
+        do {
+            $itemMismatch = Mismatch::factory()
+                ->for($import)
+                ->datatypeWikibaseItem()
+                ->create();
+            $itemMismatchQid = $itemMismatch->item_id;
+        } while ($itemMismatchQid === $dateMismatchQid);
+        do {
+            $stringMismatch = Mismatch::factory()
+                ->for($import)
+                ->datatypeString()
+                ->create();
+            $stringMismatchQid = $stringMismatch->item_id;
+        } while ($stringMismatchQid === $dateMismatchQid || $stringMismatchQid === $itemMismatchQid);
 
-        $qid = $mismatch->item_id;
-
-        $isMismatch = function (Assert $data) use ($mismatch, $import) {
-            $data->whereAll([
-                'id' => $mismatch->id,
-                // Casting values to string, as it seems that the inertia
-                // testing helper also converts all values to strings
-                'item_id' => (string) $mismatch->item_id,
-                'statement_guid' => (string) $mismatch->statement_guid,
-                'property_id' => (string) $mismatch->property_id,
-                'wikidata_value' => (string) $mismatch->wikidata_value,
-                'external_value' => (string) $mismatch->external_value,
-                'review_status' => (string) $mismatch->review_status,
-                'import_meta.external_source' => (string) $import->external_source,
-                'import_meta.user.username' => (string) $import->user->username,
-                'import_meta.created_at' => $import->created_at->toISOString(),
-                'import_meta.description' => $import->description
-            ])->etc();
+        $isMismatch = function (Mismatch $mismatch) use ($import) {
+            return function (Assert $data) use ($mismatch, $import) {
+                $data->whereAll([
+                    'id' => $mismatch->id,
+                    // Casting values to string, as it seems that the inertia
+                    // testing helper also converts all values to strings
+                    'item_id' => (string) $mismatch->item_id,
+                    'statement_guid' => (string) $mismatch->statement_guid,
+                    'property_id' => (string) $mismatch->property_id,
+                    'wikidata_value' => (string) $mismatch->wikidata_value,
+                    'external_value' => (string) $mismatch->external_value,
+                    'review_status' => (string) $mismatch->review_status,
+                    'import_meta.external_source' => (string) $import->external_source,
+                    'import_meta.user.username' => (string) $import->user->username,
+                    'import_meta.created_at' => $import->created_at->toISOString(),
+                    'import_meta.description' => $import->description
+                ])->etc();
+            };
         };
 
-        $assertLabels = function (Collection $labels) use ($mismatch) {
-            // Labels should at least have the item id and property id
-            // of a mismatch
-            return $labels->has([$mismatch->item_id, $mismatch->property_id]);
+        $assertLabels = function (Collection $labels) use (
+            $dateMismatch,
+            $dateMismatchQid,
+            $itemMismatch,
+            $itemMismatchQid,
+            $stringMismatch,
+            $stringMismatchQid
+        ) {
+            return $labels->has([
+                // labels should include item + property id of each mismatch
+                $dateMismatchQid,
+                $dateMismatch->property_id,
+                $itemMismatchQid,
+                $itemMismatch->property_id,
+                $stringMismatchQid,
+                $stringMismatch->property_id,
+                // as well as item id of an item mismatch value
+                $itemMismatch->wikidata_value,
+            ]);
         };
 
-        $withResultsPage = function (Assert $page) use ($qid, $isMismatch, $assertLabels) {
+        $withResultsPage = function (Assert $page) use (
+            $dateMismatch,
+            $dateMismatchQid,
+            $itemMismatch,
+            $itemMismatchQid,
+            $stringMismatch,
+            $stringMismatchQid,
+            $isMismatch,
+            $assertLabels
+        ) {
             $page->component('Results')
-                ->has("results.$qid.0", $isMismatch)
+                ->has("results.$dateMismatchQid.0", $isMismatch($dateMismatch))
+                ->has("results.$itemMismatchQid.0", $isMismatch($itemMismatch))
+                ->has("results.$stringMismatchQid.0", $isMismatch($stringMismatch))
                 ->where("labels", $assertLabels);
         };
 
         $response = $this->get(route('results', [
-            'ids' => $qid
+            'ids' => "$dateMismatchQid|$itemMismatchQid|$stringMismatchQid"
         ]));
 
         $response->assertSuccessful();

database/factories/MismatchFactory.php
@@ -4,9 +4,23 @@ namespace Database\Factories;
 
 use App\Models\Mismatch;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use InvalidArgumentException;
+use ValueError;
 
 class MismatchFactory extends Factory
 {
+    private const PROPERTIES = [
+        'P580' => 'time',
+        'P582' => 'time',
+        'P585' => 'time',
+        'P50' => 'wikibase-item',
+        'P86' => 'wikibase-item',
+        'P170' => 'wikibase-item',
+        'P225' => 'string',
+        'P742' => 'string',
+        'P2093' => 'string',
+    ];
+
     /**
      * The name of the factory's corresponding model.
      *
@@ -23,11 +37,42 @@ class MismatchFactory extends Factory
     {
         return [
             'statement_guid' => 'Q' . $this->faker->randomNumber() . '$' . $this->faker->uuid(),
-            'property_id' => 'P' . $this->faker->randomNumber(),
-            'wikidata_value' => $this->getRandomValue(),
+            'property_id' => $this->faker->randomElement(array_keys(self::PROPERTIES)),
+            'wikidata_value' => function (array $attributes) {
+                $propertyId = $attributes['property_id'];
+                if (!array_key_exists($propertyId, self::PROPERTIES)) {
+                    throw new ValueError("Unknown property {$propertyId}, " .
+                        'you have to specify an explicit wikidata_value');
+                }
+                return $this->getRandomValueForDatatype(self::PROPERTIES[$propertyId]);
+            },
             'external_value'=> $this->getRandomValue(),
             'external_url' => $this->faker->optional(0.6)->url()
         ];
+    }
+
+    /** Select a random property of datatype "time". */
+    public function datatypeTime(): self
+    {
+        return $this->state([
+            'property_id' => $this->getRandomPropertyIdOfDatatype('time'),
+        ]);
+    }
+
+    /** Select a random property of datatype "wikibase-item". */
+    public function datatypeWikibaseItem(): self
+    {
+        return $this->state([
+            'property_id' => $this->getRandomPropertyIdOfDatatype('wikibase-item'),
+        ]);
+    }
+
+    /** Select a random property of datatype "string". */
+    public function datatypeString(): self
+    {
+        return $this->state([
+            'property_id' => $this->getRandomPropertyIdOfDatatype('string'),
+        ]);
     }
 
     /**
@@ -63,6 +108,21 @@ class MismatchFactory extends Factory
         ]);
     }
 
+    private function getRandomValueForDatatype(string $datatype)
+    {
+        switch ($datatype) {
+            case 'time':
+                return $this->faker->date();
+            case 'wikibase-item':
+                return 'Q' . $this->faker->numberBetween(1, 10000);
+            case 'string':
+                $randomWordAmount = $this->faker->numberBetween(1, 5);
+                return $this->faker->words($randomWordAmount, true);
+            default:
+                throw new InvalidArgumentException("Unknown datatype $datatype");
+        }
+    }
+
     private function getRandomReviewStatus()
     {
         return $this->faker->randomElement([
@@ -71,5 +131,15 @@ class MismatchFactory extends Factory
             'both',
             'none'
         ]);
+    }
+
+    private function getRandomPropertyIdOfDatatype(string $datatype)
+    {
+        return $this->faker->randomElement(array_filter(
+            array_keys(self::PROPERTIES),
+            static function ($propertyId) use ($datatype) {
+                return self::PROPERTIES[$propertyId] === $datatype;
+            }
+        ));
     }
 }

commit 089ef2400dbf0cd4f2c880fa04d8c20ac732ef08

Fix missing labels on Items without mismatches
We now make sure to also retrieve the labels for those Items for which
no mismatch was found in the database.

This bug was probably introduced in #444.

Bug: T321977

app/Http/Controllers/ResultsController.php
@@ -47,9 +47,12 @@ class ResultsController extends Controller
         $propertyIds = $this->extractPropertyIds($mismatches);
         $datatypes = $wikidata->getPropertyDatatypes($propertyIds);
 
-        $entityIds = array_merge(
-            $propertyIds,
-            $this->extractItemIds($mismatches, $datatypes)
+        $entityIds = array_unique(
+            array_merge(
+                $propertyIds,
+                $this->extractItemIds($mismatches, $datatypes),
+                $requestedItemIds
+            )
         );
 
         $lang = App::getLocale();

tests/Feature/WebResultsRouteTest.php
@@ -59,6 +59,7 @@ class WebResultsRouteTest extends TestCase
         ->for(User::factory()->uploader())
         ->create();
 
+        $noMismatchQid = 'Q999979999';
         $dateMismatch = Mismatch::factory()
             ->for($import)
             ->datatypeTime()
@@ -100,6 +101,7 @@ class WebResultsRouteTest extends TestCase
         };
 
         $assertLabels = function (Collection $labels) use (
+            $noMismatchQid,
             $dateMismatch,
             $dateMismatchQid,
             $itemMismatch,
@@ -109,6 +111,7 @@ class WebResultsRouteTest extends TestCase
         ) {
             return $labels->has([
                 // labels should include item + property id of each mismatch
+                $noMismatchQid,
                 $dateMismatchQid,
                 $dateMismatch->property_id,
                 $itemMismatchQid,
@@ -127,6 +130,7 @@ class WebResultsRouteTest extends TestCase
         };
 
         $withResultsPage = function (Assert $page) use (
+            $noMismatchQid,
             $dateMismatch,
             $dateMismatchQid,
             $itemMismatch,
@@ -146,7 +150,7 @@ class WebResultsRouteTest extends TestCase
         };
 
         $response = $this->get(route('results', [
-            'ids' => "$dateMismatchQid|$itemMismatchQid|$stringMismatchQid"
+            'ids' => "$dateMismatchQid|$itemMismatchQid|$stringMismatchQid|$noMismatchQid"
         ]));
 
         $response->assertSuccessful();
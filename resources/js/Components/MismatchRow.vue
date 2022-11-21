8a4cb850d2e16660a4ea7a9c569f35448da3b725
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2022-11-21 13:09:11 +0100

    Update check for falsy values

--- resources/js/Components/MismatchRow.vue
+++ resources/js/Components/MismatchRow.vue
@@ -9,7 +9,7 @@
             </wikit-link>
         </td>
         <td :data-header="$i18n('column-wikidata-value')">
-            <span class="empty-value" v-if="!mismatch.value_label && !mismatch.wikidata_value">
+            <span class="empty-value" v-if="mismatch.value_label === null && !mismatch.wikidata_value === null">
               {{ this.$i18n('empty-value') }}
             </span> 
             <wikit-link 

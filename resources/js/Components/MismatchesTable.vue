97712a5d3745d0f3e137d05e9424abed903fd5bd
Guergana Tzatchkova <guergana.tzatchkova@wikimedia.de>
2022-11-28 16:41:39 +0100

    Move position of external-source column (#484)
    Bug: T323819

--- resources/js/Components/MismatchesTable.vue
+++ resources/js/Components/MismatchesTable.vue
@@ -5,8 +5,8 @@
                 <th class="column-property">{{$i18n('column-property')}}</th>
                 <th class="column-wikidata-value">{{$i18n('column-wikidata-value')}}</th>
                 <th class="column-external-value">{{$i18n('column-external-value')}}</th>
-                <th class="column-review-status">{{$i18n('column-review-status')}}</th>
                 <th class="column-external-source">{{$i18n('column-external-source')}}</th>
+                <th class="column-review-status">{{$i18n('column-review-status')}}</th>
                 <th class="column-upload-info">{{$i18n('column-upload-info')}}</th>
             </tr>
         </thead>
@@ -48,7 +48,7 @@ export default Vue.extend({
     .column-property { width: 12.5%; }
     .column-wikidata-value { width: 12.5%; }
     .column-external-value { width: 13.5%; }
-    .column-review-status { width: 25%; }
     .column-external-source { width: 11.5%; }
+    .column-review-status { width: 25%; }
     .column-upload-info { width: 25%; }
 </style>

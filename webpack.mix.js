ce13f00736a576ed73b565ac95db6086c2d2c0a2
Itamar Givon <itamar.givon@wikimedia.de>
2023-08-17 12:50:48 +0200

    Update build tool config

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,27 @@ const mix = require('laravel-mix');
  */
 
 mix.ts('resources/js/app.ts', 'public/js')
-    .vue({ version: 3 })
+    .vue({
+        version: 3,
+        options: {
+            // Vue 3 compat config: https://gist.github.com/thecrypticace/2e405ec70d3944a068e21caec59c7122
+            compilerOptions: {
+                compatConfig: {
+                    MODE: 2,
+                }
+            }
+        },
+    })
     .sass('resources/sass/app.scss', 'public/css')
     .copyDirectory('resources/img', 'public/images')
     .sourceMaps(false); // False prevents source maps in production
+
+mix.webpackConfig(() => {
+    return {
+        resolve: {
+            alias: {
+                vue: "@vue/compat"
+            }
+        }
+    }
+});

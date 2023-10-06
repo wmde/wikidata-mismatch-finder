23441e7f422fa2cfcce5fd6d9238c90c59f70a8f
Hasan Akgün <hasanakg@outlook.com>
2023-10-06 10:17:29 +0200

    Add custom noscript page for Javascript disabled browsers (#686)
    * Add custom noscript page for Javascript disabled browsers
    * Move noscript to a separate file and update codex-design-tokens scss import
    
    Bug: T343344

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+
+    <noscript>
+        @include('noscript')
+    </noscript>
   </head>
   <body class="app-container" dir="auto">
     @inertia

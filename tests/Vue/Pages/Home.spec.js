5a14f1220d36a6f5ebf0bb6ff26af6196324672b
Itamar Givon <itamar.givon.dev@gmail.com>
2023-11-03 18:20:00 +0100

    Remove vuex from tests

--- tests/Vue/Pages/Home.spec.js
+++ tests/Vue/Pages/Home.spec.js
@@ -1,6 +1,8 @@
 import { mount, createLocalVue } from '@vue/test-utils';
-import Vuex from 'vuex'
-import Home, { MAX_NUM_IDS } from '@/Pages/Home.vue'
+import { PiniaVuePlugin } from 'pinia';
+import { createTestingPinia } from '@pinia/testing';
+
+import Home, { MAX_NUM_IDS } from '@/Pages/Home.vue';
 
 // Stub the inertia vue components module entirely so that we don't run into
 // issues with the Head component.
@@ -16,18 +18,16 @@ describe('Home.vue', () => {
     }
 
     const localVue = createLocalVue();
-    localVue.use(Vuex);
+    localVue.use(PiniaVuePlugin);
 
     it('sanitises input with empty lines', async () => {
 
         const itemsInput = 'Q1\n\nQ2\n';
 
-        const store = new Vuex.Store({});
-
         const wrapper = mount(Home, {
             mocks,
             localVue,
-            store,
+            pinia: createTestingPinia(),
             data() {
                 return {
                     form: {
@@ -43,21 +43,24 @@ describe('Home.vue', () => {
     it('restores lastSearchIds value from store on page load', async () => {
 
         const itemsInput = 'Q1\n\nQ2\n';
-        const store = new Vuex.Store({state: {lastSearchedIds: itemsInput} });
 
         const wrapper = mount(Home, {
             mocks,
             localVue,
-            store
+            pinia: createTestingPinia({
+                initialState: {
+                    lastSearchedIds: itemsInput
+                }
+            })
         });
 
         expect( wrapper.vm.form.itemsInput ).toEqual(itemsInput);
     });
 
     it('shows dialog after clicking the more info button', async () => {
-        const store = new Vuex.Store();
+        const pinia = createTestingPinia();
 
-        const wrapper = mount(Home, { mocks, localVue, store });
+        const wrapper = mount(Home, { mocks, localVue, pinia });
         await wrapper.find('#faq-button').trigger('click');
 
         const dialog = wrapper.find('#faq-dialog .wikit-Dialog');
@@ -67,12 +70,10 @@ describe('Home.vue', () => {
     it('validates that textarea input is not empty', async () => {
         const itemsInput = '';
 
-        const store = new Vuex.Store({});
-
         const wrapper = mount(Home, {
             mocks,
             localVue,
-            store,
+            pinia: createTestingPinia(),
             data() {
                 return {
                     form: {
@@ -91,14 +92,12 @@ describe('Home.vue', () => {
     });
 
     it('validates that items in textarea input dont exceed the maximum number of ids', async () => {
-        const store = new Vuex.Store();
-
         const itemsInput = Array(MAX_NUM_IDS + 2).fill('Q21').join('\n');
 
         const wrapper = mount(Home, {
             mocks,
             localVue,
-            store,
+            pinia: createTestingPinia(),
             data() {
                 return {
                     form: {
@@ -116,14 +115,12 @@ describe('Home.vue', () => {
     });
 
     it('validates that items in textarea input are well-formed', async () => {
-        const store = new Vuex.Store();
-
         const itemsInput = 'L12345';
 
         const wrapper = mount(Home, {
             mocks,
             localVue,
-            store,
+            pinia: createTestingPinia(),
             data() {
                 return {
                     form: {
@@ -142,8 +139,8 @@ describe('Home.vue', () => {
 
     it('shows error message upon serverside validation errors', async () => {
         mocks.$page.props.errors = { 'someKey' : 'someError'}
-        const store = new Vuex.Store();
-        const wrapper = mount(Home, { mocks, localVue, store });
+        const pinia = createTestingPinia();
+        const wrapper = mount(Home, { mocks, localVue, pinia });
 
         const errorMessage = wrapper.find('#message-section .wikit-Message--error.wikit');
         expect(errorMessage.isVisible()).toBe(true);

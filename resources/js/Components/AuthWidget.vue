eff9cc906d48733b9457f9ce6391fb355d2b9311
noa <noa.rave@wikimedia.de>
2023-11-21 13:12:07 +0100

    replace links in AuthWidget

--- resources/js/Components/AuthWidget.vue
+++ resources/js/Components/AuthWidget.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="auth-widget" v-if="user">
-        <WikitLink :href="`https://www.wikidata.org/wiki/User:${user.name}`">
+        <a class="cdx-docs-link" :href="`https://www.wikidata.org/wiki/User:${user.name}`">
             <img src="images/user.svg" class="icon-user" /><span class="username">{{ user.name }}</span>
-        </WikitLink>
-        <WikitLink href="/auth/logout">{{ $i18n('log-out') }}</WikitLink>
+        </a>
+        <a class="cdx-docs-link"  href="/auth/logout">{{ $i18n('log-out') }}</a>
     </div>
-    <WikitLink v-else href="/auth/login">{{ $i18n('log-in') }}</WikitLink>
+    <a class="cdx-docs-link" v-else href="/auth/login">{{ $i18n('log-in') }}</a>
 </template>
 
 <script lang="ts">
@@ -13,20 +13,18 @@
 import type { PropType } from 'vue';
 import { defineComponent } from 'vue';
 import User from '../types/User';
-import { Link as WikitLink } from '@wmde/wikit-vue-components';
 
 export default defineComponent({
     props: {
         user: Object as PropType<User>
-    },
-    components: {
-        WikitLink
     }
 });
 </script>
 
 <style lang="scss">
 @import '~@wmde/wikit-tokens/dist/_variables.scss';
+@import '~@wikimedia/codex-design-tokens/theme-wikimedia-ui.scss';
+@import '../../sass/link.scss';
 
 .auth-widget {
     display: flex;
@@ -41,7 +39,8 @@ export default defineComponent({
         vertical-align: middle;
     }
 
-    .wikit-Link {
+    .cdx-docs-link {
+        @include cdx-mixin-link;
         white-space: nowrap;
     }
 }

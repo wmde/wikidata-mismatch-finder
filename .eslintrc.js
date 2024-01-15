161ee0d39ec89b245efce354f5f14c3b965cc3c7
gtzatchkova <guergana.tzatchkova@wikimedia.de>
2024-01-15 17:21:17 +0100

    Remove unneeded linting rules from configuration file

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,16 +12,6 @@ module.exports = {
 	rules: {
 		'max-len': [ 'error', 120 ],
 		'no-multiple-empty-lines' : ['error', { 'max': 1 }],
-		'vue/multi-word-component-names' : [ 'off' ],
-		// Migration changes
-		'vue/no-deprecated-v-bind-sync': 'off',
-		'vue/no-deprecated-slot-attribute': 'off',
-		'vue/require-explicit-emits': 'off',
-		'vue/no-deprecated-v-on-native-modifier': 'off',
-		'vue/no-deprecated-slot-scope-attribute': 'off',
-		'vue/no-v-for-template-key-on-child': 'off',
-		'vue/no-deprecated-destroyed-lifecycle': 'off',
-		'vue/no-v-model-argument': 'off',
-		'vue/no-multiple-template-root': 'off' // TODO: remove this rule in T351644
+		'vue/multi-word-component-names' : [ 'off' ]
 		},
 };
